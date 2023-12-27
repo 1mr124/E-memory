d2822126c3d9c5c9b6893fc1cab2cc49b845acc9
mr12 <221bst221@gmail.com>
2023-12-27 05:17:18 +0000

    fuck db design

--- FlaskSite/__init__.py
+++ FlaskSite/__init__.py
@@ -1,18 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_login import LoginManager
-#login_manager = LoginManager()
-#login_manager.login_view = 'auth.login'
-#def create_app(config_name):
-    # ...
-#    login_manager.init_app(app)
-    # ...
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "HARD TO KNOW"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mr124/Documents/E-memory-main/instance/site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///+PathToE-memory/instance/site.db'
 
 db = SQLAlchemy(app)
+
+# when you first create the db file u opens python and import app,db and type this 
 #with app.app_context():
 #    db.create_all()
 
