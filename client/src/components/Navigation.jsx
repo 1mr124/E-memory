9c17b8f25cffd56a7a0bf5eb6608a7497504ec96
Mr12Lab <221bst221@gmail.com>
2024-09-21 20:06:28 +0300

    Text Half Done - Multi Text add done

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -15,7 +15,7 @@ const Nav = styled.nav`
     display: flex;
     justify-content: space-around;
     width: 100%;
-    margin-bottom: 20px;
+    margin-bottom: 15px;
     background-color: #34495e; /* Background color for navigation */
     padding: 10px 0; /* Optional: add some padding for aesthetics */
     border-radius: 8px 8px 0 0; /* Rounded corners on top */
