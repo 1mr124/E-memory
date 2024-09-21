dc07525d00bd6040effac708970557430014030d
Mr12 <221bst221@gmail.com>
2024-09-21 17:38:12 +0300

    info container

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import CenterAlignedNavbar from './components/CenterAlignedNavbar';
 import BasicNavBar from './components/BasicNavBar';
 
 import './App.css';
-import Container from './components/Container';
+import InfoContainer from './components/InfoContainer';
 
 const App = () => {
   return (
     <div className="App">
       {/* <CenterAlignedNavbar /> */}
       <BasicNavBar />
-      <Container/>
+      <InfoContainer/>
     </div>
   );
 };
