34925fb5ef8b6050a754954a73b03414b947da22
Mr12Lab <221bst221@gmail.com>
2024-09-22 00:02:33 +0300

    Topic page is Done

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BasicNavBar from './components/BasicNavBar';
 import InfoContainer from './components/InfoContainer';
 import SearchContainer from './components/SearchContainer'; // Assuming you have these components
-// import TopicsContainer from './components/TopicsContainer'; // Assuming you have these components
+import TopicsContainer from './components/TopicsContainer'; // Assuming you have these components
 // import AccountContainer from './components/AccountContainer'; // Assuming you have these components
 
 import './App.css';
@@ -17,8 +17,8 @@ const App = () => {
         <Routes>
           <Route path="/info" element={<InfoContainer />} />
           <Route path="/search" element={<SearchContainer />} />
-          {/* <Route path="/topics" element={<TopicsContainer />} />
-          <Route path="/account" element={<AccountContainer />} /> */}
+          <Route path="/topics" element={<TopicsContainer />} />
+          {/* <Route path="/account" element={<AccountContainer />} />  */}
           {/* Default to InfoContainer if no path is specified */}
           <Route path="/" element={<InfoContainer />} />
         </Routes>
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Navigation = ({ activeInput, setActiveInput }) => {
+const Navigation = ({ activeInput, setActiveInput, navItems }) => {
     return (
         <Nav>
-            <NavItem onClick={() => setActiveInput('text')} active={activeInput === 'text'}>Text</NavItem>
-            <NavItem onClick={() => setActiveInput('link')} active={activeInput === 'link'}>Link</NavItem>
-            <NavItem onClick={() => setActiveInput('pics')} active={activeInput === 'pics'}>Pics</NavItem>
+            {navItems.map((item) => (
+                <NavItem
+                    key={item.value}
+                    onClick={() => setActiveInput(item.value)}
+                    active={activeInput === item.value}
+                >
+                    {item.label}
+                </NavItem>
+            ))}
         </Nav>
     );
 };
@@ -19,18 +25,14 @@ const Nav = styled.nav`
     background-color: #34495e; /* Background color for navigation */
     padding: 10px 0; /* Optional: add some padding for aesthetics */
     border-radius: 8px 8px 0 0; /* Rounded corners on top */
-    font-weight:bold;
+    font-weight: bold;
     font-size: 1.4em;
-    `;
-
+`;
 
 const NavItem = styled.div`
     cursor: pointer;
     padding: 10px 20px;
     color: ${({ active }) => (active ? '#fff' : 'black')};
-    
-    
-   
 `;
 
 export default Navigation;
