9363e4cde89b36a64e9d2428066176dfb2f7c200
Mr12Lab <221bst221@gmail.com>
2024-09-22 13:01:36 +0300

    generate config.py and ensure uploads folder exists

--- FlaskSite/__init__.py
+++ FlaskSite/__init__.py
@@ -10,6 +10,21 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
+def createDefaultConfig():
+    """
+        This will generate the config.py file if not found
+    """
+    with open("FlaskSite/config.py",'w') as f:
+        f.write("import os\n")
+        f.write("# Default configuration settings\n")
+        f.write("DEBUG = False\n")
+        f.write("SECRET_KEY = 'Make It Hard'\n")
+        f.write("BASE_DIR = os.path.abspath(os.path.dirname(__file__))\n")
+        f.write("SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'site.db')\n")
+        f.write("IMG_FOLDER = os.path.join(BASE_DIR, 'instance', 'uploads')\n")
+
+
+
 def createApp():
     app = Flask(__name__)
 
@@ -17,6 +32,10 @@ def createApp():
     # CORS(app, resources={r"/*": {"origins": "*"}})
 
     # Load configurations
+    if not os.path.exists('config.py'):
+        createDefaultConfig()
+        print(f"config.py created with default settings.")
+
     app.config.from_pyfile("config.py")
 
     # Initialize extensions
@@ -30,8 +49,12 @@ def createApp():
 
     # Ensure database tables are created (use migrations for production)
     with app.app_context():
-        # This is typically handled by migrations; use this only for initial
-        # setup
+        # This is typically handled by migrations; use this only for initial setup
+        
+        # Create uploads folder if it doesn't exist
+        uploadsFolder = app.config["IMG_FOLDER"]
+        os.makedirs(uploadsFolder, exist_ok=True)
+        
         if not os.path.exists(
             app.config["SQLALCHEMY_DATABASE_URI"].replace("sqlite:///", "")
         ):
@@ -43,6 +66,7 @@ def createApp():
                 exist_ok=True,
             )
             db.create_all()
+
     jwt = JWTManager(app)
     jwt.init_app(app)
 
