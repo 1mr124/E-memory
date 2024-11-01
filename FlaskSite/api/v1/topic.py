f63b0e0144389ad70a9a78722ff46695db2ab65b
MohamedSameh <m.samehmohamed123@gmail.com>
2024-11-01 23:12:47 +0200

    implement add new topic

--- FlaskSite/api/v1/topic.py
+++ FlaskSite/api/v1/topic.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from FlaskSite.controllers.topic_controller import add_new_topic
 from FlaskSite.forms import NewTopic
 from FlaskSite.models import db, Topic, SearchKey, Info
 
@@ -6,25 +9,17 @@ bp = Blueprint('topic', __name__)
 
 
 @bp.route('/topic/create', methods=['POST'])
+@jwt_required()
 def create_topic():
-    data = request.get_json()
+    user_id = get_jwt_identity()
+    json = request.get_json()
 
-    # Validate data
-    if not data or 'title' not in data or 'parent' not in data:
-        return jsonify({"error": "Title and parent fields are required"}), 400
+    if not all_required_exist(json, ['title']):
+        return jsonify({"error": "Title is required"}), 400
 
-    title = data['title']
-    parent = data['parent']
-
-    try:
-        # Assume we have a function `add_topic_to_db` to handle database logic
-        print("Topic added",title,"Parent: ",parent)
-        return jsonify({
-            "message": "Topic added successfully",
-        }), 201
-    except Exception as e:
-        print(f"Error adding topic to database: {e}")
-        return jsonify({"error": "Failed to add topic"}), 500
+    title = json['title']
+    parent_id = json['parent']
+    return add_new_topic(user_id=user_id, title=title, parent_id=parent_id)
 
 
 @bp.route('/delete', methods=['POST'])
@@ -60,3 +55,6 @@ def search_topic():
         return jsonify({"info": info_list, "search_keys": search_keys}), 200
     else:
         return jsonify({"message": "Topic not found"}), 404
+
+def all_required_exist(json, list_of_required_keys):
+    return all(key in json for key in list_of_required_keys)
---
+++ FlaskSite/controllers/topic_controller.py
@@ -0,0 +1,29 @@
+from flask import jsonify
+
+from FlaskSite.services.topic_service import get_topic, add_topic, get_topic_by_name
+
+
+def add_new_topic(user_id, title, parent_id):
+    try:
+        print(f'add new topic with user: {user_id}, title: {title}, parent_id: {parent_id}')
+        if title is None:
+            print ("Title is required")
+            return jsonify({"error": "Title is required"}), 400
+        if get_topic_by_name(user_id, title) is not None:
+            print(f'title {title} already exists')
+            return jsonify({"error": "Title already exists"}), 400
+
+        if parent_id is not None and get_topic(user_id, parent_id) is None:
+            print(f'parent {parent_id} for user_id {user_id} does not exist')
+            # we can set parent id to None and continue, but the user won't know what happen
+            # print(f'set parent_id to None')
+            # parent_id = None
+            return jsonify({"error": "parent_id does not exist"}), 400
+
+        add_topic(user_id, title, parent_id)
+        topic = get_topic_by_name(user_id, title)
+        print(f'topic {topic} added ')
+        return jsonify({"Message": "topic created"}), 201
+    except Exception as e:
+        print(f'exception during add new topic: {e}')
+        return jsonify({"error": "failed to insert new topic"}), 400
